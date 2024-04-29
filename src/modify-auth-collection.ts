d8f469f226a3fc284d154d998a225b3b3184e72f
WilsonLe <leanhminh2907@gmail.com>
2024-04-29 13:00:11 -0400

    refactor

--- src/modify-auth-collection.ts
+++ src/modify-auth-collection.ts
@@ -8,30 +8,38 @@ export const modifyAuthCollection = (
   existingCollectionConfig: CollectionConfig,
   subFieldName: string
 ): CollectionConfig => {
-  const existingSubField = existingCollectionConfig.fields.find(
+  // /////////////////////////////////////
+  // modify fields
+  // /////////////////////////////////////
+
+  const fields = existingCollectionConfig.fields || [];
+  const existingSubField = fields.find(
     (field) => "name" in field && field.name === subFieldName
   );
-  if (existingSubField) return existingCollectionConfig;
+  if (!existingSubField) {
+    fields.push({
+      name: subFieldName,
+      type: "text",
+      required: true,
+      unique: true,
+      index: true,
+      access: {
+        read: () => true,
+        create: () => true,
+        update: () => false,
+      },
+    });
+  }
+
+  // /////////////////////////////////////
+  // modify endpoints
+  // /////////////////////////////////////
+  const endpoints = existingCollectionConfig.endpoints || [];
+  endpoints.push(createAuthorizeEndpoint(pluginOptions));
+  endpoints.push(createCallbackEndpoint(pluginOptions));
   return {
     ...existingCollectionConfig,
-    fields: [
-      ...existingCollectionConfig.fields,
-      {
-        name: subFieldName,
-        type: "text",
-        required: true,
-        unique: true,
-        index: true,
-        access: {
-          read: () => true,
-          create: () => true,
-          update: () => false,
-        },
-      },
-    ],
-    endpoints: [
-      createAuthorizeEndpoint(pluginOptions),
-      createCallbackEndpoint(pluginOptions),
-    ],
+    fields,
+    endpoints,
   };
 };
--- src/plugin.ts
+++ src/plugin.ts
@@ -1,8 +1,5 @@
 import type { Plugin } from "payload/config";
-import { createAuthorizeEndpoint } from "./authorize-endpoint";
-import { createCallbackEndpoint } from "./callback-endpoint";
 import { modifyAuthCollection } from "./modify-auth-collection";
-import { onInitExtension } from "./onInitExtension";
 import type { PluginTypes } from "./types";
 
 export const oAuthPlugin =
@@ -10,6 +7,15 @@ export const oAuthPlugin =
   (incomingConfig) => {
     let config = { ...incomingConfig };
 
+    // If the plugin is disabled, return the config without modifying it
+    // The order of this check is important, we still want any webpack extensions to be applied even if the plugin is disabled
+    if (pluginOptions.enabled === false) {
+      return config;
+    }
+
+    // /////////////////////////////////////
+    // Modify admin panel
+    // /////////////////////////////////////
     const afterLogin = config.admin?.components?.afterLogin || [];
     if (pluginOptions.OAuthLoginButton) {
       afterLogin.push(pluginOptions.OAuthLoginButton);
@@ -26,12 +32,9 @@ export const oAuthPlugin =
       },
     };
 
-    // If the plugin is disabled, return the config without modifying it
-    // The order of this check is important, we still want any webpack extensions to be applied even if the plugin is disabled
-    if (pluginOptions.enabled === false) {
-      return config;
-    }
-
+    // /////////////////////////////////////
+    // Modify auth collection
+    // /////////////////////////////////////
     const authCollectionSlug = pluginOptions.authCollection || "users";
     const subFieldName = pluginOptions.subFieldName || "sub";
     const authCollection = config.collections?.find(
@@ -55,28 +58,6 @@ export const oAuthPlugin =
       // Add additional collections here
       modifiedAuthCollection,
     ];
-
-    config.endpoints = [
-      ...(config.endpoints || []),
-      // Add additional endpoints here
-
-    ];
-
-    config.globals = [
-      ...(config.globals || []),
-      // Add additional globals here
-    ];
-
-    config.hooks = {
-      ...(config.hooks || {}),
-      // Add additional hooks here
-    };
-
-    config.onInit = async (payload) => {
-      if (incomingConfig.onInit) await incomingConfig.onInit(payload);
-      // Add additional onInit code by using the onInitExtension function
-      onInitExtension(pluginOptions, payload);
-    };
 
     return config;
   };
