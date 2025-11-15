6501038f4b6090089e190e1cae241766073dbff4
Nikola Spalevic <nikolaspalevic@gmail.com>
2025-11-15 23:02:40 +0100

    PKCE workflow integration

--- dev/src/app/(app)/page.tsx
+++ dev/src/app/(app)/page.tsx
@@ -4,51 +4,26 @@ const Page = () => {
   return (
     <article className={["container"].filter(Boolean).join(" ")}>
       <h1>
-        Payload 3.0 <span className="rainbow">BETA</span>!
+        <span className="rainbow">Payload Oauth2 Plugin</span>
       </h1>
-      <p>
-        This BETA is rapidly evolving, you can report any bugs against{" "}
-        <a
-          href="https://github.com/payloadcms/payload-3.0-demo/issues"
-          target="_blank"
-        >
-          the repo
-        </a>{" "}
-        or in the{" "}
-        <a
-          href="https://discord.com/channels/967097582721572934/1215659716538273832"
-          target="_blank"
-        >
-          dedicated channel in Discord
-        </a>
-        .
-      </p>
-
+      <p>Features</p>
+      <ul>
+        <li>✅ Compatible with Payload v3</li>
+        <li>🔐 Configures OAuth2 with any providers</li>
+        <li>✨ Zero dependencies</li>
+        <li>⚙ Highly customizable</li>
+      </ul>
       <p>
         <strong>
-          Payload is running at <Link href="/admin">/admin</Link>
+          You can check some <Link href="/admin">SSO examples here</Link>. Make
+          sure to configure the OAuth2 plugin environment variables.
         </strong>
       </p>
-
-      <p>
-        <Link href="/my-route" target="_blank">
-          /my-route
-        </Link>{" "}
-        contains an example of a custom route running the Local API.
-      </p>
-
-      <p>You can use the Local API in your server components like this:</p>
-      <pre>
-        <code>
-          {`import { getPayload } from 'payload'
-import configPromise from "@payload-config";
-const payload = await getPayload({ config: configPromise })
-
-const data = await payload.find({
-  collection: 'posts',
-})`}
-        </code>
-      </pre>
+      <small>
+        <a href="https://github.com/payloadcms/payload" target="_blank">
+          https://github.com/payloadcms/payload
+        </a>{" "}
+      </small>
     </article>
   );
 };
--- src/default-get-token.ts
+++ src/default-get-token.ts
@@ -1,23 +1,33 @@
+import { PayloadRequest, parseCookies } from "payload";
+
 export const defaultGetToken = async (
   tokenEndpoint: string,
   clientId: string,
   clientSecret: string,
   redirectUri: string,
   code: string,
+  pkceEnabled: boolean = false,
+  req?: PayloadRequest,
 ): Promise<string> => {
+  const params = new URLSearchParams({
+    code,
+    client_id: clientId,
+    client_secret: clientSecret,
+    redirect_uri: redirectUri,
+    grant_type: "authorization_code",
+  });
+  if (pkceEnabled) {
+    const cookies = req?.headers ? parseCookies(req.headers) : null;
+    const codeVerifier = cookies?.get("pkce_verifier");
+    codeVerifier && params.append("code_verifier", codeVerifier);
+  }
   const tokenResponse = await fetch(tokenEndpoint, {
     method: "POST",
     headers: {
       "Content-Type": "application/x-www-form-urlencoded",
       Accept: "application/json",
     },
-    body: new URLSearchParams({
-      code,
-      client_id: clientId,
-      client_secret: clientSecret,
-      redirect_uri: redirectUri,
-      grant_type: "authorization_code",
-    }).toString(),
+    body: params.toString(),
   });
   const tokenData = await tokenResponse.json();
   const accessToken = tokenData?.access_token;
